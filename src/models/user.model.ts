2f4f199a02aad14d356f53993836b774db049622
italo <italoteles399@gmail.com>
2025-10-08 11:56:02 -0300

    alterado schemas de login

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -6,8 +6,13 @@ export interface User {
     email?: string;
 }
 
-export const newAuthSchema = Joi.object().keys({
+export const registerSchema = Joi.object().keys({
     name: Joi.string().max(30).trim().required(),
+    email: Joi.string().email().trim().required(),
+    password: Joi.string().min(6).max(30).trim().required(),
+});
+
+export const loginSchema = Joi.object().keys({
     email: Joi.string().email().trim().required(),
     password: Joi.string().min(6).max(30).trim().required(),
 });
